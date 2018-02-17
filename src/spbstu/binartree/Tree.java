2baf5fb1e78cae64005d131cb2dbd2819bad303b
CrazyTosser <666_mickhail@mail.ru>
2018-02-17 23:27:27 +0300

    Adding tests and debug

--- src/spbstu/binartree/Tree.java
+++ src/spbstu/binartree/Tree.java
@@ -1,13 +1,19 @@
 package spbstu.binartree;
 
+import java.util.NoSuchElementException;
+
 public class Tree {
     private Node root;
 
     /**
      * @param KeyRoot initial value for root node of new tree
      */
-    Tree(int KeyRoot) {
+    public Tree(int KeyRoot) {
         this.root = new Node(KeyRoot, null);
+    }
+
+    Tree() {
+        this.root = null;
     }
 
     /**
@@ -24,26 +30,35 @@ public class Tree {
             return Search(cur.rigth, key);
     }
 
-    public void Add(int KeyParent, int KeyChild) {
+    public void Add(int key) {
         Node x = root, y = null;
         while (x != null) {
-            if (KeyChild > KeyParent) {
+            if (key > x.value) {
                 if (x.rigth != null) {
+                    y = x;
                     x = x.rigth;
                 } else {
-                    x.rigth = new Node(KeyChild, x);
+                    x.rigth = new Node(key, y);
                     break;
                 }
             } else {
-                if (KeyChild < KeyParent) {
+                if (key < x.value) {
                     if (x.left != null) {
+                        y = x;
                         x = x.left;
                     } else {
-                        x.left = new Node(KeyChild, x);
+                        x.left = new Node(key, y);
                         break;
                     }
                 } else throw new IllegalArgumentException("Duplicate key");
             }
+        }
+
+    }
+
+    public void Add(int[] keys) {
+        for (int k : keys) {
+            this.Add(k);
         }
     }
 
@@ -58,45 +73,70 @@ public class Tree {
     }
 
     /**
-     * @param root current root of subtree
-     * @param key  wich key must be deleted
-     * @return current root of subtree (recursive)
-     */
-    private Node Remove(Node root, int key) {
-        if (root == null) {
-            return root;
-        }
-        if (key < root.value) {
-            root.left = Remove(root.left, key);
-        } else if (key > root.value) {
-            root.rigth = Remove(root.rigth, key);
-        } else if (root.left != null && root.rigth != null) {
-            root.value = Minimum(root).value;
-            root.rigth = Remove(root.rigth, root.value);
-        } else {
-            if (root.left != null)
-                root = root.left;
-            else
-                root = root.rigth;
-        }
-        return root;
-    }
-
-    /**
      * @param key that must be removed
      */
     public void Remove(int key) {
-        Remove(root, key);
+        Node cur = Search(root, key), parent = cur.parent;
+        if (cur == null) return;
+        //First variant. Cur have only left subnode
+        if (cur.rigth == null) {
+            if (parent.value > cur.value) {
+                parent.left = cur.left;
+            } else {
+
+            }
+        }
+    }
+
+    private String print(Node t) {
+        StringBuilder res = new StringBuilder();
+        if (t != null) {
+            res.append(print(t.left));
+            res.append(t.value).append(" ");
+            res.append(print(t.rigth));
+        }
+        return res.toString();
+    }
+
+    public String toString() {
+        return print(root).trim();
+    }
+
+    public Node getNode(int key) {
+        return getNode(key, 0);
+    }
+
+    /**
+     * @param key  key of searching node
+     * @param mode 0 - get node, 1 - get parent
+     *             2 - get left subnode, 3 - get right subnode
+     * @return searched node
+     */
+    public Node getNode(int key, int mode) {
+        Node res = Search(root, key);
+        if (res == null)
+            throw new NoSuchElementException("This key not found or have not parent ");
+        if (mode == 0) {
+            return res;
+        } else if (mode == 1) {
+            return res.parent;
+        } else if (mode == 2) {
+            return res.left;
+        } else {
+            return res.rigth;
+        }
     }
 
     static class Node {
         int value;
         Node parent;
-        Node left = null, rigth = null;
+        Node left, rigth;
 
         Node(int value, Node p) {
             this.value = value;
             this.parent = p;
+            rigth = null;
+            left = null;
         }
     }
 }
---
+++ src/spbstu/binartree/TreeTest.java
@@ -0,0 +1,56 @@
+package spbstu.binartree;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TreeTest {
+    private Tree test;
+
+    @Before
+    public void init() {
+        test = new Tree();
+    }
+
+    @After
+    public void tearDown() {
+        test = null;
+    }
+
+    @Test
+    public void add() {
+        test.Add(5);
+        test.Add(1);
+        test.Add(9);
+        assertEquals("1 5 9", test.toString());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void addDublicat() {
+        test.Add(1);
+        test.Add(1);
+    }
+
+    @Test
+    public void removeFst() {
+        test.Add(8); //root
+        test.Add(10); //right subtree
+        test.Add(new int[]{5, 2}); // left subtree
+        test.Remove(5);
+        assertEquals("2 8 10", test.toString());
+        assertEquals(test.getNode(8, 2).value, 2);
+    }
+
+    @Test
+    public void removeScd() {
+        test.Add(8); //root
+        test.Add(10); //right subtree
+        test.Add(new int[]{5, 2, 6, 7}); // left subtree
+        test.Remove(5);
+        assertEquals("2 6 7 8 10", test.toString());
+        assertEquals(test.getNode(8, 2).value, 6);
+    }
+
+}
