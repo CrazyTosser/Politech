d0b35890c5fd972981850ae1772f7f1bb554cfd9
crazy <666_mickhail@mail.ru>
2018-05-02 19:19:51 +0300

    Started working on siggil

---
+++ web/src/app/App.kt
@@ -0,0 +1,18 @@
+package app
+
+import logo.logo
+import react.RBuilder
+import react.RComponent
+import react.RProps
+import react.RState
+import react.dom.div
+
+class App : RComponent<RProps, RState>() {
+    override fun RBuilder.render() {
+        div("App-header") {
+            logo()
+        }
+    }
+}
+
+fun RBuilder.app() = child(App::class) {}
